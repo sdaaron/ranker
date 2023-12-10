fd92ac106a7c40506e2f11b7ea34209158cc494c
Sida Shu <sidashu233@gmail.com>
2023-12-11 03:28:17 +0800

    add

--- app/navigation.js
+++ app/navigation.js
@@ -33,6 +33,12 @@ export default function Example() {
                   </a>
                   <a
                     href="#"
+                    className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-xl font-medium text-gray-900"
+                  >
+                    历史
+                  </a>
+                  <a
+                    href="#"
                     className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-xl font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
                     频道
