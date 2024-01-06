2a3a213803984969a0e40c4852d11dc203ee28e7
sdaaron <sidashu233@gmail.com>
2024-01-07 01:15:38 +0800

    add

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body className={inter.className}>
           {children}
