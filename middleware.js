16b760887d7a78e1a1d2b4895b180ce7e2827c81
sdaaron <sidashu233@gmail.com>
2024-01-07 14:08:39 +0800

    add

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // 设置除 /admin 外的所有路由为公共路线
   publicRoutes: ["/", "/landing", "/notfound", "/*"],
+  debug: true,
 });
 
 export const config = {
