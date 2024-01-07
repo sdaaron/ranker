16388b0aa44fa21131fbc238d1a472634eff5481
sdaaron <sidashu233@gmail.com>
2024-01-07 13:54:29 +0800

    add

--- middleware.js
+++ middleware.js
@@ -1,10 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
-
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/landing"],
+  // "/" will be accessible to all users
+  publicRoutes: ["/"],
 });
 
 export const config = {
