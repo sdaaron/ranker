525dcab55e1f0baf4dc8922c76dfce4dc5fe2b57
sdaaron <sidashu233@gmail.com>
2024-01-07 13:57:49 +0800

    add

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
-export default authMiddleware({
-  // "/" will be accessible to all users
-  publicRoutes: ["/"],
-});
+// import { authMiddleware } from "@clerk/nextjs";
+// export default authMiddleware({
+//   // "/" will be accessible to all users
+//   publicRoutes: ["/"],
+// });
 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// export const config = {
+//   //   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
