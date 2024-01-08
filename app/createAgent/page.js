e8f69af40a0ffa14146b2d395dc08e6fb99ecac1
sdaaron <sidashu233@gmail.com>
2024-01-08 17:43:40 +0800

    add

--- app/createAgent/page.js
+++ app/createAgent/page.js
@@ -49,7 +49,7 @@ export default function Example() {
           </div>
         )}
 
-        <div className="p-20 flex flex-col justify-evenly items-center ">
+        <div className="p-4 flex flex-col justify-evenly items-center ">
           <div className="space-y-10">
             <div className="space-y-10 ">
               <h2 className="text-2xl font-semibold  text-gray-900">
