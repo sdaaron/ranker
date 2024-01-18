adc153ef578fabd991702a73905825852ab4eac5
sdaaron <sidashu233@gmail.com>
2024-01-18 15:35:15 +0800

    add

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -42,7 +42,7 @@ export default function Example() {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">Ranker 精选低噪信息流</strong>
+          <strong className="font-semibold">Ranker 严选信息流</strong>
           <svg
             viewBox="0 0 2 2"
             className="mx-2 inline h-0.5 w-0.5 fill-current"
