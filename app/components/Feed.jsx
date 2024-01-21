2519d4bbb4127ee2128a6a072bcfe66b13fb71dd
sdaaron <sidashu233@gmail.com>
2024-01-21 23:29:44 +0800

    add

--- app/components/Feed.jsx
+++ app/components/Feed.jsx
@@ -18,8 +18,9 @@ export default async function Example({
     .select()
     .eq("category", category)
     .eq("created_date", created_date)
-    .order("importance", { ascending: false })
-    .limit(10);
+    .order("importance", { ascending: false });
+  // .limit(10);
+  console.log(`fetching category ${category} with ${created_date}`);
   const news = data;
   if (!news || news.length === 0) {
     console.log("news: there is no news");
