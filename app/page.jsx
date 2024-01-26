71fea21e72db7ca833ab573afddd36b93f6702bd
sdaaron <sidashu233@gmail.com>
2024-01-27 02:47:28 +0800

    add

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,9 @@
 import Banner from "./components/Banner";
 import Content from "./components/Content";
 import Loadmore from "./components/Loadmore";
+import { getBeijingDate } from "./utils/DateTool";
 export default async function Home() {
-  function getFormattedDate() {
-    let now = new Date();
-    let hour = now.getHours();
-    const moment = require("moment-timezone");
-    let today = moment().tz("Asia/Shanghai").format("YYYY-MM-DD");
-    console.log("today in page: ", today);
-    return today;
-  }
-
-  // 使用函数
-  let today = getFormattedDate();
+  let today = getBeijingDate();
   console.log(today); // 输出格式为YYYY-MM-DD的日期字符串
 
   const contentData = await Content(today);
--- app/utils/DateTool.jsx
+++ app/utils/DateTool.jsx
@@ -1,51 +1,18 @@
-export function getFormattedDate(dateObj) {
-  let now = dateObj ? new Date(dateObj) : new Date();
-  let year = now.getFullYear();
-  let month = (now.getMonth() + 1).toString().padStart(2, "0");
-  let day = now.getDate().toString().padStart(2, "0");
-  return `${year}-${month}-${day}`;
+export function getBeijingDate() {
+  const moment = require("moment-timezone");
+  let today = moment().tz("Asia/Shanghai").format("YYYY-MM-DD");
+  return today;
 }
 
+export function getFormattedDate(inputDate) {
+  let date = new Date(inputDate);
+  let formattedDate = date.toISOString().split("T")[0];
+  return formattedDate;
+}
+
+console.log("getFormattedDate: ", getFormattedDate("2021-08-01T16:00:00.000Z"));
 export function getPreviousDay(dateString) {
   let previousDay = new Date(dateString);
-
   previousDay.setDate(previousDay.getDate() - 1);
   return getFormattedDate(previousDay);
 }
-// import moment from "moment-timezone";
-
-// export function getFormattedDate(dateObj) {
-//   // 使用 moment-timezone 处理日期，确保时区为东八区
-//   let now = dateObj
-//     ? moment.tz(dateObj, "Asia/Shanghai")
-//     : moment.tz("Asia/Shanghai");
-//   return now.format("YYYY-MM-DD");
-// }
-
-// export function getPreviousDay(dateString) {
-//   // 使用 moment-timezone 解析并处理日期
-//   let previousDay = moment.tz(dateString, "Asia/Shanghai").subtract(1, "days");
-//   return previousDay.format("YYYY-MM-DD");
-// }
-
-// function getFormattedDate(date) {
-//   let year = date.getFullYear();
-//   let month = date.getMonth() + 1; // 月份是从 0 开始的
-//   let day = date.getDate();
-
-//   let formattedDate =
-//     year +
-//     "-" +
-//     String(month).padStart(2, "0") +
-//     "-" +
-//     String(day).padStart(2, "0");
-
-//   return formattedDate;
-// }
-
-// function getPreviousDay(dateString) {
-//   let previousDay = new Date(dateString);
-
-//   previousDay.setDate(previousDay.getDate() - 1);
-//   return getFormattedDate(previousDay);
-// }
