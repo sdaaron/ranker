052bcdbef5c8eee3c6bc86f507b19506dfbf0e8b
sdaaron <sidashu233@gmail.com>
2024-02-04 18:08:31 +0800

    add

--- app/components/FeedGroup.jsx
+++ app/components/FeedGroup.jsx
@@ -79,26 +79,7 @@ export default async function Content(date) {
   });
 
   return (
-    <div className="content-block mb-5">
-      <header>
-        <div className="relative mx-auto max-w-3xl py-5 text-center">
-          <h1 className="mb-4 text-3xl font-extrabold text-gray-900  sm:text-4xl md:text-5xl lg:text-6xl ">
-            <span className="bg-gradient-to-r from-sky-400 to-emerald-600 bg-clip-text text-transparent">
-              Ranker
-            </span>
-            {" · "}
-            大模型日报
-            <p className="my-2 font-mono text-xl font-bold sm:my-3 sm:text-xl md:my-4 md:text-3xl lg:my-6 xl:my-8">
-              {date}
-            </p>
-          </h1>
-        </div>
-      </header>
-      <main className="main-section grid gap-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {aiData.map((data, index) => (
-          <Feed index={index} data={data} key={index} />
-        ))}
-      </main>
+    <div className="content-block xs:mb-10  sm:mb-20">
       <header>
         <div className="relative mx-auto max-w-3xl py-5 text-center">
           <h1 className="mb-4 text-3xl font-extrabold text-gray-900  sm:text-4xl md:text-5xl lg:text-6xl ">
@@ -115,6 +96,25 @@ export default async function Content(date) {
       </header>
       <main className="main-section grid gap-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {infoData.map((data, index) => (
+          <Feed index={index} data={data} key={index} />
+        ))}
+      </main>
+      <header>
+        <div className="relative mx-auto max-w-3xl py-5 text-center">
+          <h1 className="mb-4 text-3xl font-extrabold text-gray-900  sm:text-4xl md:text-5xl lg:text-6xl ">
+            <span className="bg-gradient-to-r from-sky-400 to-emerald-600 bg-clip-text text-transparent">
+              Ranker
+            </span>
+            {" · "}
+            大模型日报
+            <p className="my-2 font-mono text-xl font-bold sm:my-3 sm:text-xl md:my-4 md:text-3xl lg:my-6 xl:my-8">
+              {date}
+            </p>
+          </h1>
+        </div>
+      </header>
+      <main className="main-section grid gap-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {aiData.map((data, index) => (
           <Feed index={index} data={data} key={index} />
         ))}
       </main>
