34bea9a109672aeabb30daf29334a414c648adf4
sdaaron <sidashu233@gmail.com>
2024-07-17 22:42:48 +0800

    add

--- app/components/FeedGroup.jsx
+++ app/components/FeedGroup.jsx
@@ -56,25 +56,29 @@ export default async function Content(date) {
   function GroupLayout({ dataList, name }) {
     return (
       <>
-        <header>
-          <div className="relative mx-auto max-w-3xl py-5 text-center">
-            <h1 className="mb-4 text-3xl font-extrabold text-gray-900  sm:text-4xl md:text-5xl lg:text-6xl ">
-              <span className="bg-gradient-to-r from-sky-400 to-emerald-600 bg-clip-text text-transparent">
-                Ranker
-              </span>
-              {" · "}
-              {name}日报
-            </h1>
-            <p className="my-2 font-mono text-xl font-bold sm:my-3 sm:text-xl md:my-4 md:text-3xl lg:my-6 xl:my-8">
-              {date}
-            </p>
-          </div>
-        </header>
-        <main className="main-section grid gap-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {dataList.map((data, index) => (
-            <Feed index={index} data={data} key={index} />
-          ))}
-        </main>
+        {dataList.length > 0 && (
+          <>
+            <header>
+              <div className="relative mx-auto max-w-3xl py-5 text-center">
+                <h1 className="mb-4 text-3xl font-extrabold text-gray-900  sm:text-4xl md:text-5xl lg:text-6xl ">
+                  <span className="bg-gradient-to-r from-sky-400 to-emerald-600 bg-clip-text text-transparent">
+                    Ranker
+                  </span>
+                  {" · "}
+                  {name}日报
+                </h1>
+                <p className="my-2 font-mono text-xl font-bold sm:my-3 sm:text-xl md:my-4 md:text-3xl lg:my-6 xl:my-8">
+                  {date}
+                </p>
+              </div>
+            </header>
+            <main className="main-section grid gap-16 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {dataList.map((data, index) => (
+                <Feed index={index} data={data} key={index} />
+              ))}
+            </main>
+          </>
+        )}
       </>
     );
   }
--- app/utils/DateTool.jsx
+++ app/utils/DateTool.jsx
@@ -6,8 +6,22 @@ export function getNowInBeijing() {
   return nowInBeijing;
 }
 
+// export function getPreviousDate(date) {
+//   let previousDate = date.clone().subtract(1, "days");
+//   console.log(previousDate.format());
+//   return previousDate;
+// }
+
 export function getPreviousDate(date) {
-  let previousDate = date.clone().subtract(1, "days");
+  let previousDate;
+
+  if (date.format("YYYY-MM-DD") === "2024-07-17") {
+    // 这里是因为 6 月 17 日到 7 月 16 日之前有一个月服务下线了没有更新数据，所以中间跳过一个月的数据
+    previousDate = date.clone().subtract(32, "days");
+  } else {
+    previousDate = date.clone().subtract(1, "days");
+  }
+
   console.log(previousDate.format());
   return previousDate;
 }
